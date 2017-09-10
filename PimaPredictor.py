b4bdc4f4d1798358cd14823495a2d027e7af03bd
Yazen2017 <yazen.shunnar@gmail.com>
2017-09-10 01:06:40 -0400

    handle index columns in csv

--- PimaPredictor.py
+++ PimaPredictor.py
@@ -1,17 +1,15 @@
-import tensorflow as tf
 import pandas as pd
 from helperFunctions import *
 
-train = pd.read_csv("/Users/yazen/Desktop/datasets/PimaDiabetes/train.csv")
-test = pd.read_csv("/Users/yazen/Desktop/datasets/PimaDiabetes/test.csv")
-
-train.columns = ["pregnancy", "plasma/glucose concentration", "blood pressure","tricep skin fold thickness", "serum insulin", "body mass index", "diabetes pedigree function", "age", "label"]
-test.columns = ["pregnancy", "plasma/glucose concentration", "blood pressure","tricep skin fold thickness", "serum insulin", "body mass index", "diabetes pedigree function", "age", "label"]
+train = pd.read_csv("/Users/yazen/Desktop/datasets/PimaDiabetes/train.csv", index_col=0)
+test = pd.read_csv("/Users/yazen/Desktop/datasets/PimaDiabetes/test.csv", index_col=0)
 
 
-x = tf.placeholder("float32",[None, 10])
+x = tf.placeholder("float64",[None, 10])
 
-layer = create_network(x,[10,20,30,40,50,20,1],[tf.nn.relu,tf.nn.relu,tf.nn.relu,tf.nn.relu,tf.nn.relu,tf.nn.relu,tf.nn.sigmoid)])
+layer = create_network(x, [10, 20, 30, 40, 50, 20, 1],
+                       [tf.nn.relu, tf.nn.relu, tf.nn.relu, tf.nn.relu, tf.nn.relu, tf.nn.relu, tf.nn.sigmoid])
 
 y = tf.placeholder("float32", [None, 1])
 cost = tf.losses.log_loss(predictions=layer, labels=y)
+
--- helperFunctions.py
+++ helperFunctions.py
@@ -1,15 +1,16 @@
 import tensorflow as tf
 import numpy as np
 
+
 def create_bias(hidden_units):
     return tf.Variable(np.zeros(hidden_units))
 
 
-def create_weight(old_features,new_features):
-    return tf.Variable(np.random.randn(old_features,new_features))
+def create_weight(old_features, new_features):
+    return tf.Variable(np.random.randn(old_features, new_features))
 
 
-def create_layer(prev_layer, weight, bias, activation_function= None):
+def create_layer(prev_layer, weight, bias, activation_function=None):
     z = tf.matmul(prev_layer, weight)
     z = z + bias
 
@@ -18,8 +19,10 @@ def create_layer(prev_layer, weight, bias, activation_function= None):
 
     return z
 
+
 def create_network(input_features, units, activation_functions):
-    layer = create_layer(input_features, create_weight(units[0], units[1]), create_bias(units[1]), activation_functions[0])
+    layer = create_layer(input_features, create_weight(units[0], units[1]), create_bias(units[1]),
+                         activation_functions[0])
 
     for x in range(2, len(units)):
         layer = create_layer(layer, create_weight(units[x - 1], units[x]), create_bias(units[x]),
